e3cafebdc4cf7b7c99ad0fa3ba51c3ad2f3df38e
Julia Skobelkina <julia.skobelkina.86@gmail.com>
2022-04-20 23:57:45 +0300

    страница контиактов и каталог-табы и фильтрация

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -17,4 +17,45 @@ $(document).ready(function(){
 
   });
 
+  $('.j-tabs-link').on('click', function(event){
+    event.preventDefault();
+
+    $('.j-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.j-tabs-link');
+
+    $('.j-contacts-item').removeClass('active');
+    $('.j-contacts-item').eq(index).addClass('active');
+
+  });
+
+
+
+  $('.j-works-link').on('click', function(event){
+    event.preventDefault();
+
+    let filterData = $(this).data('filter');
+
+    console.log(filterData);
+
+    if (filterData === 'all') {
+      $('.works-item').show();
+      return;
+    }
+
+    $('.works-item').each(function(){
+
+      let typeData = $(this).data('type');
+
+      if (filterData === typeData) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+
+    });
+  });
+
+
 });
